e1018f35eb07e37feb01f6d24e98a0dd992c4423
axolotl010 <darkroad100@gmail.com>
2012-04-21 18:21:51 -0500

    firstcommit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+//session_start (); 
+//$_SESSION["ruta_actual"]= dirname(__FILE__);
+//echo $_SESSION["ruta_actual"];
+include ("controladores/SiteCtrl.php");
+
+
+
+// what the heck... trying GIT... 
+
+//echo $_SERVER['PHP_SELF'];
+
+
+?>
+
+
